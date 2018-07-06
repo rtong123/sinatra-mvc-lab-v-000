6fe7ab7e7b6264cbb146b86058746886c173afa7
Rebecca Tong <rtong@buffalo.edu>
2018-07-06 20:11:54 +0000

    save

---
+++ models/piglatinizer.rb
@@ -0,0 +1,18 @@
+class PigLatinizer
+
+attr_reader :text
+
+  def initialize
+    @text = text
+    binding.pry
+  end
+
+  def piglatinize_one_word
+
+  end
+
+  def piglatinize_sentence
+  end
+
+
+end
