a5ca93a7d6999456f89a427173702a3a438a600f
Rebecca Tong <rtong@buffalo.edu>
2018-07-09 18:36:51 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ class App < Sinatra::Base
    end
 
    post '/piglatinize' do
-    #  @analyzed_text = PigLatinizer.new(params[:user_input])
+     @analyzed_text = PigLatinizer.new(params[:user_input])
    end
 
 end
