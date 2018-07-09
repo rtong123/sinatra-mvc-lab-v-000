3df5ad0f5348a59ab1a43670b00404db910094e1
Rebecca Tong <rtong@buffalo.edu>
2018-07-09 17:30:07 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -10,6 +10,7 @@ attr_reader :text
   def piglatinize_one_word
    if text.first == a || e || i || o || u || A || E || I || O || U
      return "#{text} + 'ay'"
+     binding.pry
    else
      return text[0].strip 
   end
