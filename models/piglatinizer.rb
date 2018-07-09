612f0185a82403c31863c19da58921baef69d316
Rebecca Tong <rtong@buffalo.edu>
2018-07-09 17:27:19 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -4,11 +4,14 @@ attr_reader :text
 
   def initialize
     @text = text
-    binding.pry
+    # binding.pry
   end
 
   def piglatinize_one_word
-
+   if text.first == a || e || i || o || u || A || E || I || O || U
+     return "#{text} + 'ay'"
+   else
+     return text[0].strip 
   end
 
   def piglatinize_sentence
