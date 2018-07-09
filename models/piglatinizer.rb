ae8c0838917e0840e2fa69a54494d05dc9e0f8a9
Rebecca Tong <rtong@buffalo.edu>
2018-07-09 18:19:39 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -22,6 +22,7 @@ class PigLatinizer
       return text + 'way'
     elsif
       vowel(text[0]) == false && vowel(text[1]) == false
+      binding.pry
       first = text.slice!(0)
       second = text.slice!(1)
       return text + first + second + "ay"
