931929d3e41ded57779354932fd9e11613b9924f
Rebecca Tong <rtong@buffalo.edu>
2018-07-09 18:25:53 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -22,7 +22,6 @@ class PigLatinizer
       return text + 'way'
     elsif
       vowel(text[0]) == false && vowel(text[1]) == false && vowel(text[2]) == false
-      binding.pry
       first = text.slice!(0)
       second = text.slice!(0)
       third = text.slice!(0)
